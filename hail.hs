75eebffaee24f51efa39b37cee2e40910343ff51
Walter Tetzner <walter@waltertetzner.net>
2011-12-29 00:07:44 -0500

    Began to connect with SSL.

--- hail.hs
+++ hail.hs
@@ -6,7 +6,7 @@ import Crypto.Random
 import System.IO
 
 port = "993"
-hostname = "imap.googlemail.com"
+hostname = "imap.gmail.com"
 
 generator :: IO SystemRandom
 generator = newGenIO
@@ -16,15 +16,17 @@ connectClient hostname port = do
   g <- generator
   connectionClient hostname port
                    TLSParams { pConnectVersion = SSL3
-                             , pAllowedVersions = [SSL3]}
+                             , pAllowedVersions = [SSL3]
+                             , pCiphers = ciphersuite_all
+                             , pCompressions = []
+                             , pWantClientCert = False
+                             , pUseSecureRenegotiation = True
+                             , pUseSession = True
+                             }
                    g
 
 main = do
-  addrinfos <-  getAddrInfo Nothing (Just hostname) (Just port)
-  let imapaddr = head addrinfos
-  sock <- socket (addrFamily imapaddr) Stream defaultProtocol
-  setSocketOption sock KeepAlive 1
-  connect sock (addrAddress imapaddr)
-  --clientThing <- connectClient hostname port
-  putStrLn $ show imapaddr
+  imapclient <- connectClient hostname port
+  line <- recvData imapclient
+  return line
 
