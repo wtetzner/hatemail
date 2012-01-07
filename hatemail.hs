25091ef4412ee2046c74435c8203c1b872c40064
Walter Tetzner <walter@waltertetzner.net>
2012-01-07 15:30:47 -0500

    Print returned lazy bytestring.

--- hatemail.hs
+++ hatemail.hs
@@ -21,6 +21,7 @@ import Network.TLS
 import Network.TLS.Extra
 import Crypto.Random
 import System.IO
+import qualified Data.ByteString.Lazy as BZ
 
 port = "993"
 hostname = "imap.gmail.com"
@@ -28,16 +29,17 @@ hostname = "imap.gmail.com"
 connectClient :: String -> String -> IO (TLSCtx Handle)
 connectClient hostname port = do
   g <- newGenIO :: IO SystemRandom
-  connectionClient hostname port defaultParams { pCiphers = ciphersuite_all } g
+  client <- connectionClient hostname port defaultParams { pCiphers = ciphersuite_all } g
+  success <- handshake client
+  return client
 
 main = do
   putStr "Connecting..."
   hFlush stdout
   imapclient <- connectClient hostname port
-  success <- handshake imapclient
   putStrLn "done."
   putStr "Receiving line..."
   hFlush stdout
   line <- recvData imapclient
   putStrLn "done."
-  return line
+  BZ.putStrLn line
